ae5ea596f9d8fde97c80131b88ef37849f41b0ea
jcarlos2n <eljcarlos2n@gmail.com>
2022-12-23 12:17:36 +0100

    feat: add some query with axios

--- src/components/AccountCard/AccountCard.jsx
+++ src/components/AccountCard/AccountCard.jsx
@@ -3,19 +3,57 @@ import Container from 'react-bootstrap/Container';
 import "./AccountCard.css";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { useEffect, useState } from "react";
+import axios from 'axios';
+
 
 const AccountCard = props => {
-    console.log("soy props", props)
+    // console.log("soy props", props);
+
+    const[data, setData] = useState({});
+
+    useEffect(() => {
+        async function fetchLoans(){
+            try {
+                axios.get(`http://localhost:3001/loans/getLoans/${props.data._id}`)
+                .then(resp => {
+                    setData(resp.data.data)
+                    // console.log(resp.data)
+                })
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchLoans()
+    },[])
+
+    const LoanList = () => {
+        if (data.length > 0) {
+            // console.log("aqui si", data)
+            return(
+                <h1>HAy</h1>
+            )
+        }else{
+            // console.log("aqui no", data)
+            return(
+                <h1>no hay</h1>
+            )
+        }
+    }
+
     return (
         <Container>
-            <Card style={{ width: '18rem' }} className='cardBox'>
+            <Card className="text-center">
                 <Card.Body>
-                    <Card.Title>Cuenta</Card.Title>
+                    <Card.Title>Cuenta:</Card.Title>
                     <Card.Text>{props.data._id}</Card.Text>
                     <Card.Text><strong>Saldo: </strong>{props.data.balance}</Card.Text>
-                    {/* <Card.Text>{props.data._id}</Card.Text> */}
                     <Button variant="primary">Go somewhere</Button>
                 </Card.Body>
+                <Container>
+                    <LoanList/>
+                </Container>
+                {/* <Card.Footer className="text-muted">2 days ago</Card.Footer> */}
             </Card>
         </Container>
     )
